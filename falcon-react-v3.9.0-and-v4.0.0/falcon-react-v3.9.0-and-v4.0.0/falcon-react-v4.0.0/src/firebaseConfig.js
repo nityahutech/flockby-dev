c881fb027705dc42ce85502a858641a34f08ff13
nityahutech <106649628+nityahutech@users.noreply.github.com>
2023-04-18 10:54:05 +0530

    hiide api key

--- falcon-react-v3.9.0-and-v4.0.0/falcon-react-v3.9.0-and-v4.0.0/falcon-react-v4.0.0/src/firebaseConfig.js
+++ falcon-react-v3.9.0-and-v4.0.0/falcon-react-v3.9.0-and-v4.0.0/falcon-react-v4.0.0/src/firebaseConfig.js
@@ -5,14 +5,24 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDBPZCZOuaJldPPmlEfKoJZAEVHxMLWdpw',
-  authDomain: 'fby-dev.firebaseapp.com',
-  projectId: 'fby-dev',
-  storageBucket: 'fby-dev.appspot.com',
-  messagingSenderId: '617707689891',
-  appId: '1:617707689891:web:ac0cf29c7aba2d5210ec16',
-  measurementId: 'G-K4RQC4YGE6'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+//-------------------------
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyD5BSytx1YhNo7A6p9W1hCPI6DeYTE6dV0',
+//   authDomain: 'fby-dev.firebaseapp.com',
+//   projectId: 'fby-dev',
+//   storageBucket: 'fby-dev.appspot.com',
+//   messagingSenderId: '617707689891',
+//   appId: '1:617707689891:web:91a8d69664eacab410ec16',
+//   measurementId: 'G-8JP5V9SJ0T'
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
